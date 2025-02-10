6d54c0d2a569dafbf4de9c425aced77dee1dd1eb
Ugo Okoroafor <39538470+Uokoroafor@users.noreply.github.com>
2025-02-10 07:28:42 +0000

    feat(backend): ruff formatting changes

--- backend/src/config.py
+++ backend/src/config.py
@@ -2,16 +2,21 @@ import os
 
 
 class Settings:
-    COLLECTION_NAME: str= "todo_lists"
-    USER_COLLECTION_NAME: str="users"
-    MONGODB_URI: str= os.environ.get("MONGODB_URI")
-    SECRET_KEY: str= os.environ.get("SECRET_KEY")
-    ALGORITHM:str = os.environ.get("ALGORITHM","HS256")
-    ACCESS_TOKEN_EXPIRE_MINUTES: int = os.environ.get("ACCESS_TOKEN_EXPIRE_MINUTES",30)
-    DEBUG_MODE: bool =os.getenv("DEBUG", "True").lower() == "true"
-    DATABASE_NAME: str=os.environ.get("DATABASE_NAME")
+    COLLECTION_NAME: str = "todo_lists"
+    USER_COLLECTION_NAME: str = "users"
+    MONGODB_URI: str = os.environ.get("MONGODB_URI")
+    SECRET_KEY: str = os.environ.get("SECRET_KEY")
+    ALGORITHM: str = os.environ.get("ALGORITHM", "HS256")
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = os.environ.get(
+        "ACCESS_TOKEN_EXPIRE_MINUTES", 30
+    )
+    DEBUG_MODE: bool = os.getenv("DEBUG", "True").lower() == "true"
+    DATABASE_NAME: str = os.environ.get("DATABASE_NAME")
     HOST: str = os.getenv("HOST", "0.0.0.0")
     PORT: int = int(os.getenv("PORT", 8080))
-    WORKERS: int = int(os.getenv("WORKERS", 2))  # Uvicorn workers - will default to 1 if in debug mode
+    WORKERS: int = int(
+        os.getenv("WORKERS", 2)
+    )  # Uvicorn workers - will default to 1 if in debug mode
 
-settings=Settings()
+
+settings = Settings()
--- backend/src/dependencies.py
+++ backend/src/dependencies.py
@@ -1,6 +1,34 @@
-from fastapi import Request
+from fastapi import Request, Depends, HTTPException
+from fastapi.security import OAuth2PasswordBearer
 from crud.todo_crud import ListCRUD
+from crud.user_crud import UserCRUD
+from jose import jwt, JWTError
+from config import settings
 
 
 def get_todo_crud(request: Request) -> ListCRUD:
     return request.app.state.todo_CRUD
+
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/auth/login")
+
+
+async def get_current_user(
+    request: Request,
+    token: str = Depends(oauth2_scheme),
+):
+    # Get the UserCRUD instance from the app's state
+    user_crud: UserCRUD = request.app.state.user_CRUD
+    try:
+        payload = jwt.decode(
+            token, settings.SECRET_KEY, algorithms=[settings.ALGORITHM]
+        )
+        username: str = payload.get("sub")
+        if username is None:
+            raise HTTPException(status_code=401, detail="Invalid token")
+        user = await user_crud.get_user_by_username(username)
+        if user is None:
+            raise HTTPException(status_code=401, detail="User not found")
+        return user
+    except JWTError:
+        raise HTTPException(status_code=401, detail="Token is invalid")
