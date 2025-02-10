f5d0c81086024de3cc74b88befbc7daad6189104
Ugo Okoroafor <39538470+Uokoroafor@users.noreply.github.com>
2025-02-10 07:31:29 +0000

    feat(backend): ruff format changes

--- backend/src/routers/healthcheck_router.py
+++ backend/src/routers/healthcheck_router.py
@@ -3,17 +3,21 @@ from bson import ObjectId
 from fastapi import APIRouter, Request, HTTPException
 from schemas.todo_schemas import HealthCheckResponse
 
-router=APIRouter(prefix="/health")
+router = APIRouter(prefix="/health")
+
 
 @router.get("/")
-async def healthcheck(request:Request)->HealthCheckResponse:
+async def healthcheck(request: Request) -> HealthCheckResponse:
     try:
         # ✅ Access the database from app.state
         db = request.app.state.db
         db.test_db_connection()
 
-        return HealthCheckResponse(id=str(ObjectId()), status="healthy", when=datetime.now(),)
-    
+        return HealthCheckResponse(
+            id=str(ObjectId()),
+            status="healthy",
+            when=datetime.now(),
+        )
+
     except Exception as e:
         raise HTTPException(status_code=500, detail=f"Unhealthy: {str(e)}")
-    
