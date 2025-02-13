2eccbf522e579168696907fba32a70a353ef7f8c
Ugo Okoroafor <39538470+Uokoroafor@users.noreply.github.com>
2025-02-13 05:54:07 +0000

    feat(backend): Adding user model

---
+++ backend/src/models/user_models.py
@@ -0,0 +1,66 @@
+from bson import ObjectId
+from pydantic import BaseModel, EmailStr, Field
+from pydantic_core import core_schema
+from typing import Optional, Any
+from datetime import datetime, timezone
+
+
+# Helper to support MongoDB's ObjectId
+class PyObjectId(str):
+    @classmethod
+    def __get_pydantic_core_schema__(
+        cls, _source_type: Any, _handler: Any
+    ) -> core_schema.CoreSchema:
+        return core_schema.json_or_python_schema(
+            json_schema=core_schema.str_schema(),
+            python_schema=core_schema.union_schema(
+                [
+                    core_schema.is_instance_schema(ObjectId),
+                    core_schema.chain_schema(
+                        [
+                            core_schema.str_schema(),
+                            core_schema.no_info_plain_validator_function(
+                                cls.validate
+                            ),
+                        ]
+                    ),
+                ]
+            ),
+            serialization=core_schema.plain_serializer_function_ser_schema(
+                lambda x: str(x)
+            ),
+        )
+
+    @classmethod
+    def validate(cls, value) -> ObjectId:
+        if not ObjectId.is_valid(value):
+            raise ValueError("Invalid ObjectId")
+
+        return ObjectId(value)
+
+
+# Pydantic model for User
+class User(BaseModel):
+    id: Optional[PyObjectId] = Field(
+        default_factory=lambda: PyObjectId(ObjectId()), alias="_id"
+    )
+    username: str
+    email: EmailStr
+    hashed_password: str
+    is_active: bool = True
+    created_at: datetime = Field(
+        default_factory=lambda: datetime.now(tz=timezone.utc)
+    )
+
+    class Config:
+        population_by_name = True
+        json_encoders = {ObjectId: str}
+        json_schema_extra = {
+            "example": {
+                "username": "johndoe",
+                "email": "johndoe@example.com",
+                "hashed_password": "hashed_password_here",
+                "is_active": True,
+                "created_at": "2024-12-31T23:59:59.999Z",
+            }
+        }
