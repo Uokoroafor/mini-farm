1961ffea7f892970c2599820e10cd48a154aea85
Ugo Okoroafor <39538470+Uokoroafor@users.noreply.github.com>
2025-02-18 06:05:29 +0000

    feat(frontend): Updating ToDo Contexts

--- frontend/src/contexts/ToDoContext.jsx
+++ frontend/src/contexts/ToDoContext.jsx
@@ -1,33 +1,66 @@
-import React, { createContext, useState, useEffect } from "react";
+import React, { createContext, useState, useEffect, useContext } from "react";
 import api from "../api";
 
-export const ToDoContext = createContext();
+const ToDoContext = createContext();
 
 export const ToDoProvider = ({ children }) => {
   const [listSummaries, setListSummaries] = useState([]);
+  const [selectedItem, setSelectedItem] = useState(null);
 
   useEffect(() => {
-    reloadData();
+    reloadData().catch(console.error);
   }, []);
 
-  const reloadData = async () => {
+  async function reloadData() {
+    try{
     const response = await api.get(`/api/lists`);
-    setListSummaries(response.data);
-  };
+    setListSummaries(response.data);} catch (error) {
+      console.error(error);
+    }
+  }
 
-  const createList = async (name) => {
-    await api.post(`/api/lists`, { name });
-    reloadData();
-  };
+  function handleNewToDoList(newName) {
+    const updateData = async () => {
+      const newListData = {
+        name: newName,
+      };
 
-  const deleteList = async (id) => {
-    await api.delete(`/api/lists/${id}`);
-    reloadData();
-  };
+      await api.post(`/api/lists`, newListData);
+      reloadData().catch(console.error);
+    };
+    updateData();
+  }
+
+  function handleDeleteToDoList(id) {
+    const updateData = async () => {
+      await api.delete(`/api/lists/${id}`);
+      reloadData().catch(console.error);
+    };
+    updateData();
+  }
+
+  function handleSelectList(id) {
+    console.log("Selecting item", id);
+    setSelectedItem(id);
+  }
+
+  function backToList() {
+    setSelectedItem(null);
+    reloadData().catch(console.error);
+  }
 
   return (
-    <ToDoContext.Provider value={{ listSummaries, reloadData, createList, deleteList }}>
+    <ToDoContext.Provider value={{ listSummaries,
+      selectedItem,
+      handleSelectList,
+      handleNewToDoList,
+      handleDeleteToDoList,
+      backToList,}}>
       {children}
     </ToDoContext.Provider>
   );
 };
+
+export function useToDo() {
+  return useContext(ToDoContext);
+}
