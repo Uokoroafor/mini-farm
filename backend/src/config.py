4b265b63e675fccf535d203eb3abddaa88444f65
Ugo Okoroafor <39538470+Uokoroafor@users.noreply.github.com>
2025-02-23 06:32:55 +0000

    feat(backend): Styling changes for config.py

--- backend/src/config.py
+++ backend/src/config.py
@@ -7,9 +7,7 @@ class Settings:
     MONGODB_URI: str = os.environ.get("MONGODB_URI")
     SECRET_KEY: str = os.environ.get("SECRET_KEY")
     ALGORITHM: str = os.environ.get("ALGORITHM", "HS256")
-    ACCESS_TOKEN_EXPIRE_MINUTES: int = os.environ.get(
-        "ACCESS_TOKEN_EXPIRE_MINUTES", 30
-    )
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = os.environ.get("ACCESS_TOKEN_EXPIRE_MINUTES", 30)
     DEBUG_MODE: bool = os.getenv("DEBUG", "True").lower() == "true"
     DATABASE_NAME: str = os.environ.get("DATABASE_NAME")
     HOST: str = os.getenv("HOST", "0.0.0.0")
